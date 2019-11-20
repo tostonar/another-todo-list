1ee1d3ce3e4a371d567dad414ad140b642b623bf
Andrea T <tostonar@gmail.com>
2019-11-20 10:24:37 -0500

    Test onchange

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -3,7 +3,7 @@ import React from 'react'
 function TodoItem(props) {
   return (
     <div className="todo-item">
-      <input type="checkbox" checked={props.todo.completed} />
+      <input type="checkbox" checked={props.todo.completed} onChange={()=>console.log("I've been checked!") />
       <p>{props.todo.text}</p>
     </div>
   )
