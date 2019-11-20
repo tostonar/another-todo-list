a621db01dae59dfd83a573ade5d4d5b78ffacfde
Andrea T <tostonar@gmail.com>
2019-11-20 11:47:54 -0500

    do not change state of todo directly when checked

--- src/App.js
+++ src/App.js
@@ -14,7 +14,10 @@ class App extends React.Component {
     this.setState(prevState => {
       const updatedTodos = prevState.todos.map(todo => {
         if (todo.id === id) {
-          todo.completed = !todo.completed
+          return {
+            ...todo,
+            completed: !todo.completed
+          }
         }
         return todo
       })
