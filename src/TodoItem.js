03cfbbc780210372f6b9811e9cff980b4e0546df
Andrea T <tostonar@gmail.com>
2019-11-25 09:16:53 -0500

    style completed tasks

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,6 +1,12 @@
 import React from 'react'
 
 function TodoItem(props) {
+  const completedStyle = {
+    fontStyle: "itlaic",
+    color: "#cdcdcd",
+    textDecoration: "line-through"
+  }
+
   return (
     <div className="todo-item">
       <input
@@ -8,7 +14,7 @@ function TodoItem(props) {
         checked={props.todo.completed}
         onChange={() => props.handleChange(props.todo.id)}
       />
-      <p>{props.todo.text}</p>
+      <p style={props.item.completed ? completedStyle : null}>{props.todo.text}</p>
     </div>
   )
 }
